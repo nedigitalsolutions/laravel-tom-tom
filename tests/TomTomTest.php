45aa38f4e163ca77ad67cf4ef3997c9b5d205db2
Jonathan Martin <jono20201@gmail.com>
2016-10-14 18:34:20 +0100

    Fix issues that prevent PHPUnit been successful

--- tests/TomTomTest.php
+++ tests/TomTomTest.php
@@ -4,7 +4,7 @@ namespace Hirealite\LaravelTomTom\Tests;
 
 use Hirealite\LaravelTomTom\TomTom;
 
-class TomTomTests extends TestCase
+class TomTomTest extends TestCase
 {
     /** @test */
     public function it_can_be_instantiated()
